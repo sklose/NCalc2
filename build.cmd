072dcd9cd361852ac7c53941125ab8fefa370db3
Sebastian Klose <mail@sklose.com>
2016-01-24 15:54:14 -0500

    Testing appveyor build without KoreBuild

--- build.cmd
+++ build.cmd
@@ -1,8 +1,7 @@
 @ECHO OFF
-dnu restore --quiet
-
-dnvm use 1.0.0-rc1-update1 -r clr
-dnx -p test\NCalc.Tests test -appveyor
-
-dnvm use 1.0.0-rc1-update1 -r coreclr
-dnx -p test\NCalc.Tests test -appveyor
+call dnu.cmd restore --quiet
+call dnvm.cmd use 1.0.0-rc1-update1 -r clr
+call dnx -p test\NCalc.Tests test -appveyor
+call dnvm.cmd use 1.0.0-rc1-update1 -r coreclr
+call dnx -p test\NCalc.Tests test -appveyor
+call dnu pack src\NCalc --quiet --configuration Release
