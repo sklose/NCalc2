89ff8fa4518420121b47802c84bae9ccdbe88993
Sebastian Klose <mail@sklose.com>
2016-07-24 15:40:29 -0400

    Update appveyor script

--- build.cmd
+++ build.cmd
@@ -1,7 +1,4 @@
 @ECHO OFF
-call dnu.cmd restore --quiet
-call dnvm.cmd use 1.0.0-rc1-update1 -r clr
-call dnx -p test\NCalc.Tests test -appveyor
-call dnvm.cmd use 1.0.0-rc1-update1 -r coreclr
-call dnx -p test\NCalc.Tests test -appveyor
-call dnu pack src\NCalc --quiet --configuration Release
+dotnet restore
+dotnet test test\NCalc.Tests -appveyor
+dotnet pack src\NCalc --configuration Release
