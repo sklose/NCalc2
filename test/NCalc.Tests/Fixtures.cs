c637f46a8b124dbf2458b178398c1815719e8107
ABykiev <bykiev.andrei@mail.ru>
2024-03-19 16:09:11 +0300

    Update Fixtures.cs

--- test/NCalc.Tests/Fixtures.cs
+++ test/NCalc.Tests/Fixtures.cs
@@ -225,9 +225,11 @@ namespace NCalc.Tests
             Assert.Equal(1d, Extensions.CreateExpression("Floor(1.5)").Evaluate());
             Assert.Equal(-1d, Extensions.CreateExpression("IEEERemainder(3,2)").Evaluate());
             Assert.Equal(0d, Extensions.CreateExpression("Log(1,10)").Evaluate());
+            Assert.Equal(1d, Extensions.CreateExpression("Log(10,10)").Evaluate());
             Assert.Equal(0d, Extensions.CreateExpression("Ln(1)").Evaluate());
-            Assert.Equal(Math.Log(0.5), Extensions.CreateExpression("Ln(0.5)").Evaluate());
+            Assert.Equal(2.30258509299405, Extensions.CreateExpression("Ln(10)").Evaluate());
             Assert.Equal(0d, Extensions.CreateExpression("Log10(1)").Evaluate());
+            Assert.Equal(1d, Extensions.CreateExpression("Log10(10)").Evaluate());
             Assert.Equal(9d, Extensions.CreateExpression("Pow(3,2)").Evaluate());
             Assert.Equal(3.22d, Extensions.CreateExpression("Round(3.222,2)").Evaluate());
             Assert.Equal(-1, Extensions.CreateExpression("Sign(-10)").Evaluate());
